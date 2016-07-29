66f1a7b57d8662bf8c19f28e2afa30f7c1d264a7
2016-王念 <1245343496@qq.com>
2016-07-30 00:28:59 +0800

    黑马项目，手机安全卫士

---
+++ app/src/main/java/com/waynian/mobilephonesafe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.waynian.mobilephonesafe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
